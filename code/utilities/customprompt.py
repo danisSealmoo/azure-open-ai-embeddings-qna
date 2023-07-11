37f00a5e513b4dd911488954cb8b01784af15e66
danisSealmoo <136306086+danisSealmoo@users.noreply.github.com>
2023-07-11 16:45:32 +0000

    prompt

--- code/utilities/customprompt.py
+++ code/utilities/customprompt.py
@@ -2,11 +2,17 @@
 from langchain.prompts import PromptTemplate
 
 template = """{summaries}
-Please reply to the question using only the information present in the text above. 
-Include references to the sources you used to create the answer if those are relevant ("SOURCES"). 
-If you can't find it, reply politely that the information is not in the knowledge base.
+请只使用summaries提供的信息回答问题。如果适用，包括对用于创建答案的来源的引用（"来源"）。如果在summaries中找不到信息，请礼貌地回复该信息不在知识库中。使用用户提问的语言回复。
 Question: {question}
-Answer:"""
+Answer:
+Tags:[]"""
+
+# template = """{summaries}
+# Please reply to the question using only the information present in the text above. 
+# Include references to the sources you used to create the answer if those are relevant ("SOURCES"). 
+# If you can't find it, reply politely that the information is not in the knowledge base.
+# Question: {question}
+# Answer:"""
 
 PROMPT = PromptTemplate(template=template, input_variables=["summaries", "question"])
 
